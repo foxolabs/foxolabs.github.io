83347b49c52c31b5da156663bb6fe865c33e421d
foxolabs <sayora.ventures@gmail.com>
2017-05-27 22:16:37 -0400

    Add files via upload
    changes as of 05/28

---
+++ js/type.js
@@ -0,0 +1,11 @@
+$(function(){
+  $("#typed").typed({
+    stringsElement: $('#typed-s'),
+    strings: ["Startup. Lean MVP Model", "Creative Mobile Apps", "Progressive Web Apps", "Cloud Solutions","Cyber Security"],
+    //contentType: 'html',
+    backDelay: 600,
+    loop: true,
+    startDelay: 900,
+    typeSpeed: 180
+  });
+});
